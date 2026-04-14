90b5273d12d571d4870e3a41e9e324bab7b49aac
Telly Reames <tellyreames2@gmail.com>
2026-04-13 17:51:46 -0700

    fix: add force_delete to ECR repository to unblock re-deploys

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "main" {
   name                 = "${var.project_name}-api"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
